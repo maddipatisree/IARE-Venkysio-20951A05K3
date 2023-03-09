ac21e964dc969081100ed783b00b93b2065cc6d0
maddipatisree <127425934+maddipatisree@users.noreply.github.com>
2023-03-09 21:57:10 +0530

    Create update reversal.java

---
+++ update reversal.java
@@ -0,0 +1,24 @@
+mport java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);  
+        String str = obj.nextLine(); 
+
+      reversal ("Sneha");
+        reversal (str);
+    }
+
+    public static void reversal(String str){
+        if (str.length() > 0){
+        System.out.print(str.charAt(str.length()-1));
+        reversal(str.substring(0,str.length()-1));
+    }
+        else{
+            System.out.println();
+            System.out.println("Done");
+        }
+
+
+    }
+}
